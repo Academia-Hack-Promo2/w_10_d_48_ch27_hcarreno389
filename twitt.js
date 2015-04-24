7841bc71ef163a576f04cc0f57b65ce110595d7b
Hector <hector12j@gmail.com>
2015-04-24 15:28:08 -0430

    casi listo solo falta style

--- twitt.js
+++ twitt.js
@@ -1,5 +1,4 @@
-usuarios = new Array;
-usuario = new Array;
+
 
 
 function tabla() {
@@ -8,12 +7,25 @@ function tabla() {
   var titulo   = document.createElement("div");
   var textoCelda = document.createTextNode("Twitts");
   titulo.appendChild(textoCelda);
-  
   var c_twitt = document.createElement("div")
   var twitt_new = document.createElement("button");
   var tletra = document.createTextNode("Publicar Twitt");
-  var barra = document.createElement("text");
+  var barra = document.createElement("input");
   c_twitt.appendChild(barra);
+
+  // var twitt = document.getElementsByTagName("input")[0].value;
+  // console.log(twitt);
+  twitt_new.onclick = function(){
+    var twitts = document.createElement("div");
+    console.log(twitts);
+    var twitt = document.createTextNode(barra.value);
+    twitts.appendChild(twitt);
+    //twitts.innerHTML(barra.value);
+  console.log(barra.value);
+   
+  body.appendChild(twitts);
+
+  };
 
  
  
@@ -21,7 +33,8 @@ function tabla() {
   body.appendChild(titulo);
   twitt_new.appendChild(tletra);
   body.appendChild(twitt_new);
+  body.appendChild(c_twitt);
   titulo.setAttribute("border", "3");
+  
 }
 tabla();
-console.log(usuario);
